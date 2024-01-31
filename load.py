3738be8cd7c7de2dfc897a61c7879ed5e669ca58
C. Divisha <chowdavaramdivisha@gmail.com>
2024-01-31 13:34:28 -0500

    Created load.py file

---
+++ load.py
@@ -0,0 +1,10 @@
+import main
+import extract
+import sqlalchemy
+import pandas as pd 
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+from datetime import datetime
+import datetime
+import sqlite3
